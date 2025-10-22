92cc0c70bfdc2600aae3e7c9159851bb4622d9cf
shashank-77-77 <shashankmankar412@gmail.com>
2025-10-22 13:35:29 +0530

    DAY3 DIWALI ASSIGNMENT

---
+++ Day3/Addition.cpp
@@ -0,0 +1,17 @@
+// Addition of two numbers 
+// Input: a, b 
+// Output: a + b
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int sum = a + b;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}   
